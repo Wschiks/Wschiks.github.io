9dce001c161685c44d11463af55d649d81f2e48f
Wschiks <1206720@student.roc-nijmegen.nl>
2024-04-02 10:56:06 +0200

    betsanden toegevoegd aan github.

---
+++ java.js
@@ -0,0 +1,9 @@
+function clearFields() {
+    // Zoek de input en textarea elementen op
+    var inputField = document.getElementById('inputField');
+    var textareaField = document.getElementById('textareaField');
+  
+    // Leeg de inhoud van de input en textarea velden
+    inputField.value = '';
+    textareaField.value = '';
+  }
